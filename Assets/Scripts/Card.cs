0d13f7604b7b9443c9b5d15865114093b6186de9
Keyrune <spyro-99@mail.ru>
2021-10-12 14:32:22 +0300

    initial commit

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Card : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
+{
+    private Camera mainCamera;
+    private Vector3 offset;
+    public Transform defaultParent;
+
+    void Awake()
+    {
+        mainCamera = Camera.allCameras[0];
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        offset = transform.position - mainCamera.ScreenToWorldPoint(eventData.position);
+
+        defaultParent = transform.parent;
+
+        transform.SetParent(defaultParent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 newPosition;
+        newPosition = mainCamera.ScreenToWorldPoint(eventData.position);
+        newPosition.z = 0f; 
+        transform.position = newPosition + offset;
+
+    }
+
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(defaultParent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+    
+    
+
+}
---
+++ Assets/Scripts/Dungeon.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class Dungeon : MonoBehaviour, IDropHandler
+{
+    public Obstacle[] obstacles = new Obstacle[16];
+
+    private void Awake()
+    {
+        //for (int i = 0; i < obstacles.Length; i++)
+        //{
+        //    obstacles[i] = emptyObstacle;
+        //}
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Card card = eventData.pointerDrag.GetComponent<Card>();
+
+        if (card) 
+        {
+            card.defaultParent = transform;
+
+            
+            
+            //ActivateCard();
+            
+        }
+            
+
+
+    }
+}
